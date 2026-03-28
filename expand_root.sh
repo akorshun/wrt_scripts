9f8e77a214c104e2322a256ee146420179440eab
akorshun <razor1702@gmail.com>
2026-03-28 18:42:41 +0300

    Create expand_root.sh

---
+++ expand_root.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install packages
+apk update
+apk add parted losetup resize2fs blkid
+ 
+# Download expand-root.sh
+wget -U "" -O expand-root.sh "https://openwrt.org/_export/code/docs/guide-user/advanced/expand_root?codeblock=0"
+ 
+# Source the script (creates /etc/uci-defaults/70-rootpt-resize and /etc/uci-defaults/80-rootpt-resize, and adds them to /etc/sysupgrade.conf so they will be re-run after a sysupgrade)
+. ./expand-root.sh
+ 
+# Resize root partition and filesystem (will resize partiton, reboot resize filesystem, and reboot again)
+sh /etc/uci-defaults/70-rootpt-resize
