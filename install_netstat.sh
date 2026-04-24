e63b99614dd798492dbb1cc95e5a614835d481b1
akorshun <razor1702@gmail.com>
2026-04-24 22:15:37 +0300

    Update install_netstat.sh

--- install_netstat.sh
+++ install_netstat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget  --no-check-certificate -O /tmp/netstat.apk "https://github.com/nooblk-98/luci-app-netstat/releases/download/v1.1.4-19/luci-app-netstat-1.1.4-r19.apk"
+wget  --no-check-certificate -O /tmp/netstat.apk "https://github.com/nooblk-98/luci-app-netstat/releases/download/v1.1.5-20/luci-app-netstat-1.1.5-r20.apk"
 apk --allow-untrusted add /tmp/netstat.apk
 rm /tmp/netstat.apk
